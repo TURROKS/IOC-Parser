9d7a8a8ae2d903120b769b939159acd47e2f93d0
Mario Rojas <mariro_ch@hotmail.com>
2019-11-02 16:41:22 +0400

    Updated Project Files

--- extractor.py
+++ extractor.py
@@ -3,27 +3,27 @@ from modules import email_extract,file_extract,hash_extract,ip_extract,url_extra
 
 def main():
 
-    with open('/Users/Mario/Documents/Git/IOC-Parser/tests/output.txt', 'w') as out:
+    with open('output.txt', 'w') as out:
 
         # Calls hash extract function
-        with open('/Users/Mario/Documents/Git/IOC-Parser/tests/test.txt', 'r') as inp:
-            hash_extract.main(inp,out)
+        with open('input.txt', 'r') as inp:
+            hash_extract.main(inp, out)
 
         # Calls ip extract function
-        with open('/Users/Mario/Documents/Git/IOC-Parser/tests/test.txt', 'r') as inp:
-            ip_extract.main(inp,out)
+        with open('input.txt', 'r') as inp:
+            ip_extract.main(inp, out)
 
         # Calls url extract function
-        with open('/Users/Mario/Documents/Git/IOC-Parser/tests/test.txt', 'r') as inp:
-            url_extract.main(inp,out)
+        with open('input.txt', 'r') as inp:
+            url_extract.main(inp, out)
 
         # Calls email extract function
-        with open('/Users/Mario/Documents/Git/IOC-Parser/tests/test.txt', 'r') as inp:
-            email_extract.main(inp,out)
+        with open('input.txt', 'r') as inp:
+            email_extract.main(inp, out)
 
         # Calls file extract function
-        with open('/Users/Mario/Documents/Git/IOC-Parser/tests/test.txt', 'r') as inp:
-            file_extract.main(inp,out)
+        with open('input.txt', 'r') as inp:
+            file_extract.main(inp, out)
 
 
 if __name__ == '__main__':
